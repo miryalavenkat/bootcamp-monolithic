bd7daab1cb468d5d70a88d93ce17f8e8e495868f
miryalavenkat <venkatnarayana.m@zemoso.com>
2023-07-25 15:33:01 +0530

    resolving cross origin error

--- backend/src/main/java/com/zemoso/greencommute/controller/UserController.java
+++ backend/src/main/java/com/zemoso/greencommute/controller/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin("http://localhost:8080")
+@CrossOrigin("http://localhost:8000")
 @RequestMapping("v1/saved-jobs")
 public class UserController {
 
